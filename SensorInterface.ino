cf50eaa0be4b855a4724c5e154b959430c554ca3
Kevin Kaldvansvik <kevin.k.kaldvansvik@gmail.com>
2019-01-29 15:45:53 +0100

    SD card module now works

--- SensorInterface.ino
+++ SensorInterface.ino
@@ -3,16 +3,25 @@
     1. Setting up serial and i2c communication
     2. Setting up the BME280 altitude sensor and printing the desired sensor information
 */
+
 #include <Wire.h>
 #include <SPI.h>
+#include <SD.h>
 #include "src/LED/LED.h"
 #include "src/BME280/SparkFunBME280.h"
-//#include "src/SD-1.2.3/src/SD.h"
 #include "src/Adafruit_BNO055/Adafruit_BNO055.h"
 
 //Setting up objects and params
 BME280 Bme;
 int const diode = 13;
+uint8_t const SD_cspin = BUILTIN_SDCARD;
+File myFile;
+String DataFile = "Datafile.txt";
+struct SensorData {
+  float temp;
+  float pressure;
+  float altitude;
+};
 
 void setup()
 {
@@ -30,18 +39,68 @@ void setup()
     while(1); //Freeze
   }
 
+  //Setup SD-card module
+  if (!SD.begin(SD_cspin)) {
+    Serial.println("initialization failed!");
+    delay(1000);
+    return;
+  }
+  else {
+    delay(1000);
+    Serial.println("SD card module successfully started");
+    delay(2000);
+    
+  }
+
   //Successfull setup lights diode
   LEDConfig(diode);
   LEDSetMode(diode, HIGH);
+
+
+  //Delete file?
+  Serial.println("Type 'd'/'k' to delete or keep log file ");
+  
+  while(1){
+    String answer;
+    answer = Serial.read(); 
+    if (answer == "d"){
+      SD.remove("Datafile.txt");
+      break;
+    }
+    else if (answer == "k") {
+      break;
+    }
+  }
 }
 
 void loop()
-{
-
+{ 
+  //init data struct
+  SensorData data;
+  data.temp = Bme.readTempC();
+  data.pressure = Bme.readFloatPressure();
+  data.altitude = Bme.readFloatAltitudeMeters();
+  
+  //Writing to SD card
+  myFile = SD.open("Datafile.txt", FILE_WRITE);
+  if (myFile) {
+    myFile.println(createDataString(data));
+  }
+  else {
+    Serial.println("Error opening file");
+  }
+  myFile.close();
+  
   Bme.printSensorInformation(true,true,true,false);
-  Serial.println(Bme.readFloatAltitudeMeters());
-  //readTempC()
-  //readFloatPressur()
-
   delay(100);
+}
+
+String createDataString(SensorData data){
+  String dataString = "";
+  dataString += String(data.temp);
+  dataString += ",";
+  dataString += String(data.pressure);
+  dataString += ",";
+  dataString += String(data.altitude);
+  return dataString;
 }
