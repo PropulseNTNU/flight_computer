d989492278e706dd450a9ab8f7e8c9a362c1571a
Patrick Nitschke <47362946+patricknitschke@users.noreply.github.com>
2019-02-28 17:15:41 +0100

    Update recovery_main.cpp

--- src/FSM/utilities/recovery/recovery_main.cpp
+++ src/FSM/utilities/recovery/recovery_main.cpp
@@ -6,10 +6,6 @@
 
 #include <PWMServo.h>
 
-#define MainChuteALT 457
-#define DrogueServoPin 2
-#define MainServoPin 3
-
 PWMServo drogueServo;
 PWMServo mainServo;
 
@@ -28,90 +24,24 @@ UpdateAltitude* p_customAltitude = (UpdateAltitude*) malloc(sizeof(UpdateAltitud
 ParachuteStateStruct* p_parachute = &parachute;  //Define parachute after struct
 AltitudeStateStruct* p_altitude = &alt;
 UpdateAltitude* p_customAltitude = &customAlt;
- 
 
+//initialise Servo and start position
+mainServo.attach(MainServoPin);
+mainServo.write(85);
+drogueServo.attach(DrogueServoPin);
+drogueServo.write(85);
 
+//initialise parachute states
+p_parachute->drogueDeployed = false;
+p_parachute->mainDeployed = false;
+p_parachute->mainStopped = false;
 
-void setup() {
-    Serial.begin(9600);
-    
-    
-    //initialise Servo and start position
-    mainServo.attach(MainServoPin);
-    mainServo.write(85);
-    drogueServo.attach(DrogueServoPin);
-    drogueServo.write(85);
-    
-    
-    //initialise parachute states
-    p_parachute->drogueDeployed = false;
-    p_parachute->mainDeployed = false;
-    p_parachute->mainStopped = false;
-    
-    
-    //initialise altitude state
-    p_altitude->isFalling = false;   //skal peke noen andre steder
-    p_altitude->currentAltitude = 0;
-    p_altitude->maxAltitude = -1;
-    
-    
-    //initialise altitude simulator
-    p_customAltitude->manual_descend = false;
-}
+//initialise altitude state
+p_altitude->isFalling = false;   //skal peke noen andre steder
+p_altitude->currentAltitude = 0;
+p_altitude->maxAltitude = -1;
 
+//initialise altitude simulator
+p_customAltitude->manual_descend = false;
 
-void loop() {
-    while (!p_parachute->mainStopped) {
-        
-        //Read altitude here
-        p_altitude->currentAltitude = p_customAltitude->altitudeUpdate(p_altitude->currentAltitude, p_customAltitude->manual_descend);
-        
-        
-        //Update variables
-        if (p_altitude->currentAltitude > p_altitude->maxAltitude) {
-            p_altitude->maxAltitude = p_altitude->currentAltitude;
-        }
-        Serial.print("Altitude: ");
-        Serial.println(p_altitude->currentAltitude);
-        
-        
-        //Update orientation
-        if (p_altitude->currentAltitude >= 3000) {
-            p_customAltitude->manual_descend = true;
-            Serial.println("Descending!");
-        }
-        
-        
-        //Check conditions
-        if ((!p_altitude->isFalling) && (p_altitude->currentAltitude <= (p_altitude->maxAltitude-20))) {
-            p_altitude->isFalling = true;
-            Serial.println("Falling!");
-        }
-        
-        if ((!p_parachute->drogueDeployed) && (p_altitude->isFalling) && (p_altitude->currentAltitude <= (p_altitude->maxAltitude-25))) {
-            drogueServo.write(5);     //clock-wise
-            p_parachute->drogueDeployed = true;
-            Serial.println("Drogue!");
-        }
-        
-        if ((!p_parachute->mainDeployed) && (p_parachute->drogueDeployed) && (p_altitude->currentAltitude <= MainChuteALT)) {
-            mainServo.write(0);     //clock-wise
-            p_parachute->mainDeployed = true;
-            Serial.println("Main!");
-        }
-        
-        if ((p_parachute->mainDeployed) && (p_altitude->currentAltitude <= (MainChuteALT-200))) {
-            mainServo.write(85);    //Stop
-            p_parachute->mainStopped = true;
-            Serial.println("Main stop!");
-        }
-        
-        delay(100);
-    }
-    /*
-    free(p_parachute);
-    free(p_altitude);
-    free(p_customAltitude);
-    */
-}
 
