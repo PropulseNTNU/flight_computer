138e3a022f1b31e1aa46b4b4cff839be6274a791
Patrick Nitschke <47362946+patricknitschke@users.noreply.github.com>
2019-02-28 16:55:45 +0100

    Update apogee_state.cpp

--- src/FSM/states/apogee_state.cpp
+++ src/FSM/states/apogee_state.cpp
@@ -1,9 +1,19 @@
 using namespace std;
 #include "../states.h"
+#include "../utilities/recovery.h"
+#include "apogee_state.h"
 #include <Arduino.h>
+#include <PWMServo.h>
 
 int apogee_state(double data[]) {
 	return_code ret_code;
 	ret_code = REPEAT;
 	return ret_code;
 }
+
+PWMServo drogueServo;
+
+drogueServo.attach(DrogueServoPin);
+
+drogueServo.write(5);     //clock-wise
+p_parachute->drogueDeployed = true;
