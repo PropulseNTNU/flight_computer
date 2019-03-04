41a770cfa0318d0e21cea61220056c44ff836537
OleB <ole.eithun.pedersen@gmail.com>
2019-03-04 17:20:40 +0100

    Adds first version of GPS class with test.

--- src/GPS/gps.cpp
+++ src/GPS/gps.cpp
@@ -1,15 +1,107 @@
 #include "gps.h"
+#include "nmeacommands.h"
 
-GPS::GPS() 
+GPS& GPS::getInstance() 
 {
-    buffer = new char* [bufferSentences*82]; // Max length of NMEA sentence is 82 bits
-    baudrate = GPS_BAUDRATE;
-    gpsSerial = new Serial4;
+    static GPS instance;
+    instance.gpsSerial = &Serial4;
+    instance.baudrate = GPS_BAUDRATE;
+    instance.gpsSerial->begin(instance.baudrate);
+    return instance;
 }
 
-void serialEvent4()
-/* This function is called every time new GPS-data is 
-   available at the Serial4 port */
+void GPS::sendCommand(const char *command)
 {
+    if (!*gpsSerial)
+        return;
+    gpsSerial->println(command);
+}
 
+void GPS::stop() 
+{
+    gpsSerial->end();
+}
+
+void GPS::start()
+{
+    if (!*gpsSerial)
+        gpsSerial->begin(baudrate);
+}
+
+void GPS::setUpdateRate(unsigned int updateRate)
+{
+    if (!*gpsSerial)
+        return;
+    switch (updateRate) {
+        case 1:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_100_MILLIHERTZ);
+            break;
+        case 2:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_200_MILLIHERTZ);
+            break;
+        case 10:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_1HZ);
+            break;
+        case 20:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_2HZ);
+            break;
+        case 50:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_5HZ);
+            break;
+        case 100:
+            gpsSerial->println(PMTK_SET_NMEA_UPDATE_10HZ);
+        default:
+            break;
+    }
+    
+}
+
+double GPS::getAltitude() 
+{
+    return altitude.meters();
+}
+
+double GPS::getLatitude() 
+{
+    return location.lat();
+}
+
+double GPS::getLongitude() 
+{
+    return location.lng();
+}
+
+double GPS::getSpeed() 
+{
+    return speed.mps();
+}
+
+uint16_t GPS::getYear() 
+{
+    return date.year();
+}
+
+uint8_t GPS::getMonth() 
+{
+    return date.month();
+}
+
+uint8_t GPS::getDay() 
+{
+    return date.day();
+}
+
+uint8_t GPS::getHour()
+{
+    return time.hour();
+}
+
+uint8_t GPS::getMinute() 
+{
+    return time.minute();
+}
+
+uint8_t GPS::getSecond() 
+{
+    return time.second();
 }
--- src/GPS/gps.h
+++ src/GPS/gps.h
@@ -1,8 +1,9 @@
 /* Simple the wrapper for the TinyGPS++ library with an
-implementation of the serialEvent4() for reading
+implementation of serialEvent4() for reading
 raw NMEA data from a MTK3339 GPS module. It is intended
 to be used in the flight computer of the Sleipner rocket
 created by Propulse NTNU. 
+- Ole Bjørn Eithun Pedersen 2019.
 
 This code is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
@@ -21,39 +22,55 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "TinyGPS++.h"
 
-
 #ifndef BUFFER_SENTENCES
 #define BUFFER_SENTENCES 4
+#endif
 
 #ifndef GPS_BAUDRATE
 #define GPS_BAUDRATE 9600
 #endif
 
+#ifndef GPS_MAX_WAIT_MILLI
+#define GPS_MAX_WAIT_MILLI 200
+#endif
+
+/* The GPS class is implemented with the singelton design pattern. It
+guarantees that only one instance of the class can exist at any given
+time. */
 class GPS : public TinyGPSPlus 
 {
 public:
-    GPS(); // Constructor
+    static GPS& getInstance(); // Get pointer to the only instance
     
+    void sendCommand(const char *command); // Send command
+
     // Set functions
     void setUpdateRate(unsigned int updateRate);
     void stop(); // Stops the automatic updating of the GPS
-
+    void start(); // Starts the automatic updating if turned off
     // Get functions
-    double getAlltitude(); // Returns alltitude in meters
+    double getAltitude(); // Returns alltitude in meters
     double getLatitude();
     double getLongitude();
     double getSpeed(); // Returns speed in m/s
 
     uint16_t getYear();
-    unint8_t getMonth();
+    uint8_t getMonth();
     uint8_t getDay();
 
-private:
-    char* buffer[]; // Buffer for NMEA sentences
-    unsignend int bufferSentences; // Number of NMEA sentences to store in buffer
+    uint8_t getHour();
+    uint8_t getMinute();
+    uint8_t getSecond();
 
-    Serial4* gpsSerial; /* Serial object for uart communication with gps.
-                           Serial4, which corresponds to pins Rx->31 Tx->32
-                           on the Teensy3.6, is used on the flight computer. */
-    unsignend int baudrate; // Baudrate for GPS
+private:
+    GPS() {}
+    GPS(GPS const&);
+    void operator=(GPS const&);
+    char* nmeaBuffer[]; // Buffer for NMEA sentences
+    unsigned int bufferSentences; // Number of NMEA sentences to store in buffer2
+
+    HardwareSerial* gpsSerial; /* Serial object for uart communication with gps.
+                           Serial4 must be used, pins Rx->31 Tx->32 on the Teensy3.6,
+                           is used on the flight computer. */
+    unsigned int baudrate; // Baudrate for GPS
 };
