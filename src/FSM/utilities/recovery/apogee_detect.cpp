23148c08f87199f19046fce9623e29e0eb4af620
Patrick Nitschke <patrick.nitschke8@gmail.com>
2019-03-09 10:28:58 +0100

    added apogee_detect files

---
+++ src/FSM/utilities/recovery/apogee_detect.cpp
@@ -0,0 +1,8 @@
+//
+//  apogee_detect.cpp
+//  
+//
+//  Created by Patrick Nitschke on 05/03/2019.
+//
+
+#include "apogee_detect.hpp"
---
+++ src/FSM/utilities/recovery/apogee_detect.hpp
@@ -0,0 +1,13 @@
+//
+//  apogee_detect.hpp
+//  
+//
+//  Created by Patrick Nitschke on 05/03/2019.
+//
+
+#ifndef apogee_detect_hpp
+#define apogee_detect_hpp
+
+#include <stdio.h>
+
+#endif /* apogee_detect_hpp */
