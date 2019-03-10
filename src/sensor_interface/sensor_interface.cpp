d658caaaa60f6e70b556e93d95798fa6a0a9c175
Kevin Kaldvansvik <kevin.k.kaldvansvik@gmail.com>
2019-03-10 11:55:06 +0100

    fixed bug

--- src/sensor_interface/sensor_interface.cpp
+++ src/sensor_interface/sensor_interface.cpp
@@ -8,23 +8,6 @@ using namespace std;
 BME280 Bme;
 Adafruit_BNO055 IMU = Adafruit_BNO055(100, IMU_ADDRESS);
 
-/*
-    Note that the IMU has declared x axis as the yaw axis, the y axis as the
-    pitch axis and the z axis as the roll axis. This is corrected as:
-      roll  = x axis
-      pitch = y axis
-      yaw   = z axis
-*/
-
-const uint8_t IMU_ADDRESS = 0x28;
-
-/*
-    Initialization of the BME and IMU sensor
-    BME sensor initial address 0x77, if jumper is added address is 0x76
- */
-BME280 Bme;
-Adafruit_BNO055 IMU = Adafruit_BNO055(100, IMU_ADDRESS);
-
 BME280* get_BME(){
   return &Bme;
 }
--- src/sensor_interface/sensor_interface.h
+++ src/sensor_interface/sensor_interface.h
@@ -5,6 +5,8 @@
 #include "BME280/SparkFunBME280.h"
 #include "sensor_data.h"
 
+const uint8_t IMU_ADDRESS = 0x28;
+
 /*
     Note that the IMU has declared x axis as the yaw axis, the y axis as the
     pitch axis and the z axis as the roll axis. This is corrected as:
