8b91e7951a2cc5a0a11a60fbed3beb05ae9a2d81
havard <hloevi@live.no>
2019-03-30 12:44:50 +0100

    Updated kalmanfilter and lookup-table to the latest versions

--- src/FSM/utilities/airbrakes/kalman.cpp
+++ src/FSM/utilities/airbrakes/kalman.cpp
@@ -6,9 +6,9 @@ using namespace BLA;
 Matrix<2,2> A_d;
 Matrix<2,1> B_d;
 const Matrix<1,2> C_d={1,0};
-const Matrix<2,2> E_d={0.00001,0.0000002,0.000000004,0.0000003};
-const Matrix<2,2> Q={300,0 ,0 ,0.000000001};
-const float R=1000; //Litt usikker på om R skal være en matrise eller en skalar. trodde egentlig den skulle være en matrise.
+const Matrix<2,2> E_d={0.01, 0, 0, 0.01};
+const Matrix<2,2> Q={90, 0 ,0 , 1000};
+const float R=1; //Litt usikker på om R skal være en matrise eller en skalar. trodde egentlig den skulle være en matrise.
 const Matrix<2,2> I={1,0,0,1};
 Matrix<2,2> P_k_bar={1,0,0.1,0};
 Matrix<2,1>x_hat_bar={0,280};// skal egentlig være ca.{2000,300};
@@ -43,5 +43,5 @@ void kalman(float* estimates, float  altitude, float acceleration, float dt, flo
  
   //project ahead--------------------------------------------------------------
   x_hat_bar = A_d * x_hat + B_d * (acceleration);
-  P_k_bar = A_d * P_k * (~A_d) + Q; //Mulig Q skal byttes ut med: E_d * Q * (~E_d); Uten pådrag blir det bare Q;
+  P_k_bar = A_d * P_k * (~A_d) + E_d*Q*(~E_d); //Mulig Q skal byttes ut med: E_d * Q * (~E_d); Uten pådrag blir det bare Q;
 }
