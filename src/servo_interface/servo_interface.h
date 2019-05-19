607e77826e2354444dcc3566cec86abbccb8ee02
Patrick Nitschke <patrick.nitschke8@gmail.com>
2019-05-19 15:29:13 +0200

    fix servo pins and main chute angles

--- src/servo_interface/servo_interface.h
+++ src/servo_interface/servo_interface.h
@@ -3,14 +3,14 @@
 #include <Servo.h>
 
 #define AIRBRAKES_SERVO_PIN 9
-#define DROGUE_SERVO_PIN 2
-#define MAIN_SERVO_PIN 3
+#define DROGUE_SERVO_PIN 3
+#define MAIN_SERVO_PIN 4
 
 // Deploy and reset angles refer to servo positions, defined here as reset angles are required in flight_computer.ino
 #define DROGUE_DEPLOY_ANGLE 30
 #define DROGUE_RESET_ANGLE 145
-#define MAIN_DEPLOY_ANGLE 5
-#define MAIN_RESET_ANGLE 165
+#define MAIN_DEPLOY_ANGLE 165
+#define MAIN_RESET_ANGLE 5
 
 enum servoEnum {
     AIRBRAKES_SERVO, DROGUE_SERVO, MAIN_SERVO, NUM_SERVOS
