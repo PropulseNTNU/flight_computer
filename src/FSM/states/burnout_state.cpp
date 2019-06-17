59dac2d0f9bfb48b2d99b33265b4791b2c1b0cd5
Kevin Kaldvansvik <kevin.k.kaldvansvik@gmail.com>
2019-06-17 15:34:25 +0200

    fixed burnout transition

--- src/FSM/states/burnout_state.cpp
+++ src/FSM/states/burnout_state.cpp
@@ -1,11 +1,11 @@
 #include "../states.h"
 
-double last_acc_z = 0;
-int increasing_count = 0; // add one to this count each time the rocket acc_z is increasing but negative
+double last_data_x = 0;
+int increasing_count = 0; // add one to this count each time the rocket acc_x is increasing but negative
 int increasing_min_limit = 10; // then rocket needs to have increased its accelleration for increasing_min_limit iterations 
 int burntime = 0; // the time the motor has burned
-const int burnout_time = 4000; // Milliseconds the rough estimate of how long the motor will burn
-const int epsilon = 200; // a small time constatnt used for max and min deviation from burnout_time
+const int burnout_time = 7000; // Milliseconds the rough estimate of how long the motor will burn
+const int epsilon = 500; // a small time constatnt used for max and min deviation from burnout_time
 double launchTime = 0; // the time at launch(motor start)
 
 int burnout_state(double data[]) {
@@ -17,21 +17,33 @@ int burnout_state(double data[]) {
 	}
 	burntime = data[TIMESTAMP] - launchTime;
 
+	Serial.println(burntime);
+
 	//if acc_z is negative but increasing
-	if(data[ACC_Y] > last_acc_z && last_acc_z < 0){
+	if(data[ACC_X] > last_data_x && last_data_x < 0){
 		// increment the increasing count
 		increasing_count += 1;
 	}
-	last_acc_z = data[ACC_Y];
+	last_data_x = data[ACC_X];
 
 	// if data[ACC_Z] is negative but increasing, and time is more than burntime - epsilon
 	// or time is more than burntime + epsilon which is the uppper limit.
-	if ((increasing_count > increasing_min_limit && burnout_time > (burnout_time - epsilon)) || (burntime > burnout_time + epsilon)) {
+	/*if (((increasing_count > increasing_min_limit) && (burnout_time > (burnout_time - epsilon)))
+	   || (burntime > burnout_time + epsilon)) {
 		ret_code = NEXT;
 	}
 	else {
 		ret_code = REPEAT;
 	}
+	*/
+
+	if (burntime > (burnout_time + epsilon) && data[ACC_X] <= 0){
+		ret_code = NEXT;
+	}
+	else {
+		ret_code = REPEAT;
+	}
+
 
 	return ret_code;
 }
