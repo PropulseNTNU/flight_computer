21fb5c54fd3423190247f855886b2acfb523feef
jaf111 <jan_aleksander@hotmail.com>
2019-06-17 18:32:09 -0600

    Update sensorsFromPayload.h

--- src/bluetooth/sensorsFromPayload.h
+++ src/bluetooth/sensorsFromPayload.h
@@ -11,9 +11,7 @@
  * Currently 15 sensor readings
 */
 enum sensorDataBle { TIME, TROLL, TPITCH, TYAW, 
-                 TACCELERATION_X, TACCELERATION_Y, TACCELERATION_Z,
-                 TCOMPASS_X, TCOMPASS_Y, TCOMPASS_Z ,
-                 TEMPERATURES , THUMIDITY, TALTITUDE, TPRESSURE, TGAS, TEMP_ACCUR_SENS, NUMBER_OF_SENSORS};
+                 TACCELERATION_X, TACCELERATION_Y, TALTITUDE};
 
 
 #endif
