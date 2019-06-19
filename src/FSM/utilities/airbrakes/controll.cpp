4c5a1bad43e44674e57d292e8ddf71d3f564432a
hermandrage <hermandr@stud.ntnu.no>
2019-06-19 10:48:37 -0600

    Regulerer prosent istedenfor

--- src/FSM/utilities/airbrakes/controll.cpp
+++ src/FSM/utilities/airbrakes/controll.cpp
@@ -1,11 +1,27 @@
 #include "controll.h"
-
+#include <math.h>
 float integrate(float prev_sum, float value, float step){
   return prev_sum + (value * step);
 }
 
+float pros_to_deg(float prosent){
+  return asin(prosent/100) * 180.0/PI;
+}
+
 float controller(float* error, ControlParameters* parameters, float* riemann_sum, float dt){ //PI-controller
   // add a lower and upper bound to prevernt overflow
-  *riemann_sum = integrate(*riemann_sum, *error, dt); //integrates error
-  return (parameters->kpp*(*error)) + (parameters->kpi*(*riemann_sum));
+  if(integrate(*riemann_sum, *error, dt) <= 40 && integrate(*riemann_sum, *error, dt) > -40){
+    *riemann_sum = integrate(*riemann_sum, *error, dt); //integrates error
+  }
+
+  float prosent = (parameters->kpp*(*error)) + (parameters->kpi*(*riemann_sum));
+  if (prosent > 86.6){
+    return 60;
+  }
+  else if(prosent<0.0){
+    return 0;
+  }
+  else {
+    return pros_to_deg(prosent);
+  }
 }
