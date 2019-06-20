82564bf80fd2a7fa2551614d37f129d98f9eacae
havard <hloevi@live.no>
2019-06-20 01:04:34 -0600

    Moved stopping gps to idle state

--- src/FSM/states/armed_state.cpp
+++ src/FSM/states/armed_state.cpp
@@ -1,5 +1,5 @@
 #include "../states.h"
-#include "../../sensor_interface/sensor_interface.h"
+
 
 int armed_state(double data[]) {
 	return_code ret_code;
@@ -7,7 +7,6 @@ int armed_state(double data[]) {
 	Serial.println("Waiting for x-accel input: must be larger than 15");
 
 	if (data[LINEAR_ACCEL_X] > 15.0) {
-		get_GPS()->stop();
 		ret_code = NEXT;
 	}
 	else {
