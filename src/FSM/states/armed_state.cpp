bf601c260bbc2135f443f16c408fd51853c236de
havard <hloevi@live.no>
2019-06-21 00:24:36 -0600

    Fix servo initial position

--- src/FSM/states/armed_state.cpp
+++ src/FSM/states/armed_state.cpp
@@ -1,10 +1,12 @@
 #include "../states.h"
+#include "../../servo_interface/servo_interface.h"
 
 
 int armed_state(double data[]) {
 	return_code ret_code;
 
 	Serial.println("Waiting for x-accel input: must be larger than 15");
+	get_servo(AIRBRAKES_SERVO)->write(0);
 
 	if (data[LINEAR_ACCEL_X] > 15.0) {
 		ret_code = NEXT;
--- src/FSM/states/idle_state.cpp
+++ src/FSM/states/idle_state.cpp
@@ -1,9 +1,12 @@
 #include "../states.h"
 #include "../../sensor_interface/sensor_interface.h"
+#include "../../servo_interface/servo_interface.h"
 
 //Only the arm button should trigger next state
 int idle_state(double data[]) {
 	return_code ret_code;
+
+	get_servo(AIRBRAKES_SERVO)->write(0);
 
 	if (digitalRead(ARM_BUTTON_PIN)){
 		get_GPS()->stop();
